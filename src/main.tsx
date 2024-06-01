a25b533cba4cd787130f640b2535455d2fb191ac
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-06-01 15:41:51 +0330

    basic init routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import LandingPage from './components/pages/LandingPage.tsx';
+import LoginPage from './components/pages/LoginPage.tsx';
+import SignupPage from './components/pages/SignupPage.tsx';
+import EditProfilePage from './components/pages/EditProfilePage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>
+  },
+  {
+    path: "/editprofile",
+    element: <EditProfilePage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
